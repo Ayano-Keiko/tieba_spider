f248293acea31175c6394de64aaf03e83b6a5195
Bina <107738723+NihaoMonde@users.noreply.github.com>
2023-09-28 23:00:39 +0800

    Update spider_tieba_gener.py
    将保存格式修改

--- spider_tieba_gener.py
+++ spider_tieba_gener.py
@@ -1,13 +1,16 @@
 '''
-爬取XX吧首页
+爬取刀剑神域吧首页
 
 '''
 import re
 import time
+import urllib.error
+import urllib.request
+import urllib
 from urllib import request,parse
-import openpyxl
 from bs4 import BeautifulSoup
 import os
+import csv
 
 class Spider:
     def __init__(self):
@@ -25,10 +28,10 @@ class Spider:
         :return:
         '''
 
-        response = request.Request(url,headers=self.headers)
-        r = request.urlopen(response)
+        response = urllib.request.Request(url,headers=self.headers)
+        r = urllib.request.urlopen(response)
         html = r.read().decode("UTF-8")
-        r.close()
+        
         return html
 
 
@@ -51,7 +54,7 @@ class Spider:
         :param html: 网页源代码
         :param items: 保存位置，列表
         :param temps: 元组形式，传入时间，内容分别为年月日
-        :return: items
+        :return: [[num,topicName,topicUrl,conFinal,author,t_f], [num,topicName,topicUrl,conFinal,author,t_f], ...] 
         '''
 
         # 通过正则表达式匹配指定信息
@@ -95,88 +98,37 @@ class Spider:
             items.append([num,topicName,topicUrl,conFinal,author,t_f])
         return items
 
-    def saveData(self, data, name, num):
-        file = name + str(num) + ".xlsx"
-        dir = name + "吧"
+    def saveData(self, data, name): # 3rd param num
+        '''
+        '''
+        if not os.path.exists(name):
+            os.mkdir(name)
 
-        # 创建目录存放数据，如果目录已经存在则不创建
-        if os.path.exists(dir):
-            pass
-        else:
-            os.mkdir(dir)
+        filename = os.path.join(name, name + "(less)" + ".csv")
+        fp = open(filename, "a+", encoding="GB18030", newline="")
+        
+        writer = csv.writer(fp, delimiter=",")
+        # headers
+        # writer.writerow(["Reviews", "Title","URL","content","楼主","Time"])
 
-        try:
-            # 判断xls文件是否存在，不存在则创建，存在则追加数据
-            if os.path.exists(file):
-                wb_write = openpyxl.load_workbook(dir+"/"+file)
-                ws_write = wb_write[name]
+        for row in data:
+            writer.writerow(row)
 
-                nrows = ws_write.max_row  # 行数
-                ncols = ws_write.max_column  # 列数
-                for i in range(len(data)):
-                    ws_write.cell(i + 1 + nrows, 1, data[i][1])  # [1]
-                    ws_write.cell(i + 1 + nrows, 2, data[i][0])  # [0]
-                    ws_write.cell(i + 1 + nrows, 3, data[i][2])  # [2]
-                    ws_write.cell(i + 1 + nrows, 4, data[i][3])  # [3]
-                    ws_write.cell(i + 1 + nrows, 5, data[i][4])  # [4]
-                    ws_write.cell(i + 1 + nrows, 6, data[i][5])  # [5]
-                wb_write.save(dir+"/"+file)
-            else:
-                wb = openpyxl.Workbook()
-                ws = wb.create_sheet(name)
-
-                ws.cell(1, 1, "楼层名称")  # [1]
-                ws.cell(1, 2, "评论数")  # [0]
-                ws.cell(1, 3, "楼层地址")  # [2]
-                ws.cell(1, 4, "大致内容")  # [3]
-                ws.cell(1, 5, "楼主")  # [4]
-                ws.cell(1, 6, "时间")  # [5]
-                for i in range(len(data)):
-                    ws.cell(i + 2, 1, data[i][1])  # [1]
-                    ws.cell(i + 2, 2, data[i][0])  # [0]
-                    ws.cell(i + 2, 3, data[i][2])  # [2]
-                    ws.cell(i + 2, 4, data[i][3])  # [3]
-                    ws.cell(i + 2, 5, data[i][4])  # [4]
-                    ws.cell(i + 2, 6, data[i][5])  # [5]
-
-                wb.save(dir+"/"+file)
-        except ValueError:
-            wb_new = openpyxl.Workbook()
-            ws_new = wb_new.create_sheet(name)
-
-            ws_new.cell(1, 1, "楼层名称")  # [1]
-            ws_new.cell(1, 2, "评论数")  # [0]
-            ws_new.cell(1, 3, "楼层地址")  # [2]
-            ws_new.cell(1, 4, "大致内容")  # [3]
-            ws_new.cell(1, 5, "楼主")  # [4]
-            ws_new.cell(1, 6, "时间")  # [5]
-            for i in range(len(data)):
-                ws_new.cell(i + 2, 1, data[i][1])  # [1]
-                ws_new.cell(i + 2, 2, data[i][0])  # [0]
-                ws_new.cell(i + 2, 3, data[i][2])  # [2]
-                ws_new.cell(i + 2, 4, data[i][3])  # [3]
-                ws_new.cell(i + 2, 5, data[i][4])  # [4]
-                ws_new.cell(i + 2, 6, data[i][5])  # [5]
-
-            wb_new.save(dir + "/" + file)
-        finally:
-            print("已存储第{}页的数据".format(num+1))
+        fp.close()
 
 
-def main():
-    '''
-    
-    :return: None
-    '''
+
+if __name__ == "__main__":
     # 要爬取的贴吧(自行输入想要爬取的吧名)
-    word = "汽车"
+    word = "刀剑神域"
     kw = word.encode("UTF-8")  # 将关键词转换为Bytes格式，URL不支持中文格式
     # 贴吧页数（根据贴吧的页数自行改变）
-    page = 100
+    page = 20 # 100
     # 基础网址
     baseurl = "https://tieba.baidu.com/"
     # 将元素保存至item中
     items = []
+    data = []
 
     # 获取时间，用于获取当天的日期并返回年月日
     local_time = time.localtime(time.time())
@@ -187,19 +139,20 @@ def main():
     tb = Spider()
 
     for i in range(page + 1):
-        # url拼接，由于搜索关键词是中文，所以需要进行处理
-        url = baseurl+"f?kw="+parse.quote(kw) + "&ie=utf-8&pn=" + str(i * 50)
-        html = tb.getContent(url)
-        
-        # 储存帖子信息
-        data = tb.getInfo(html,items,year,month,day)
-        
+        try:
+            # url拼接，由于搜索关键词是中文，所以需要进行处理
+            url = baseurl+"f?kw="+parse.quote(kw) + "&ie=utf-8&pn=" + str(i * 50)
+            html = tb.getContent(url)
+            
+            # 储存帖子信息
+            data.extend(tb.getInfo(html,items,year,month,day))
+            
+            # 每次爬取完停止1s，防止由于爬取速度过快被限制访问
+            # time.sleep(1)
+        except urllib.error.URLError as e:
+            print("{}".format(e))
+        finally:
+            print("Finish {}".format(i))
+    
         # 将数据储存
-        tb.saveData(data, word, i)
-
-        # 每次爬取完停止数秒，防止由于爬取速度过快被限制访问
-        time.sleep(3)
-
-
-if __name__ == "__main__":
-    main()
+        tb.saveData(data, word)
